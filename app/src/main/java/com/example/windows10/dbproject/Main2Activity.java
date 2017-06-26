6e02560eb20fc232db443b6e409edb100f915b6f
Muhibush <ukiacrew@gmail.com>
2017-06-26 21:42:10 +0700

    layout menu

--- app/src/main/java/com/example/windows10/dbproject/Main2Activity.java
+++ app/src/main/java/com/example/windows10/dbproject/Main2Activity.java
@@ -1,17 +1,28 @@
 package com.example.windows10.dbproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        title = (TextView) findViewById(R.id.title_tujuan);
-        title.setText(getIntent().getStringExtra("crime details"));
+    }
+    public void listicon(View view){
+        Intent intent = new Intent(this,RecyclerActivity.class);
+        startActivity(intent);
+    }
+    public void mapicon(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+    public void addicon(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
